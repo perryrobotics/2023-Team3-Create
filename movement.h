0eb96979f31ca696572d8d31026a99f65a2c1bca
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2023-04-10 13:08:56 -0500

    Final .h files

---
+++ movement.h
@@ -0,0 +1,7 @@
+void forward(int speed, int mm);
+void backward(int speed, int mm);
+void CCW(int speed, int angle);
+void CW(int speed, int angle);
+void setup();
+void move_servo_slowly(int port, int end_pos, int delay);
+void forward_to_bump(int speed);
